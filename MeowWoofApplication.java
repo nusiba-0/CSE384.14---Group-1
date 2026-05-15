999115f2eed9b21a2f5ee9bc3ada40f0ed3dff6f
Nusiba <155464810+nusiba-0@users.noreply.github.com>
2026-05-16 03:16:19 +0600

    Add Nusiba- Application code

---
+++ MeowWoofApplication.java
@@ -0,0 +1,17 @@
+package org.example.meowwoof;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MeowWoofApplication {
+
+
+   public static void main(String[] args) {
+       SpringApplication.run(MeowWoofApplication.class, args);
+   }
+
+
+}
