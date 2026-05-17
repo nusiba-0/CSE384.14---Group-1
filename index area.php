522ec84853579891eb0759ea23775ca12f6594d4
Md. Nazmul Hassan <nazmulhf58@gmail.com>
2026-05-17 15:53:28 +0600

    Delete index area.php

--- index area.php
+++
@@ -1,6 +0,0 @@
-INSERT INTO Areas (area_name, location_description) VALUES
-('Mirpur', 'Near road side and residential area'),
-('Dhanmondi', 'Lake side and park area'),
-('Uttara', 'Sector based residential area'),
-('Mohammadpur', 'Town hall and bus stand area'),
-('Banani', 'Commercial and lake area');
